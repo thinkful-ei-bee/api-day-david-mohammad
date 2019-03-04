2e90dc9b0be592d4c2a2b5c016d9963f25f739f7
David Bolin <david@bolin.at>
2019-03-04 09:46:16 -0800

    get items and add item connected to api

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,15 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
+
 });
 
-store.items.push(Item.create('apples'));
 

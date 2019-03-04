bf12b4e364b7c655232740edb4ebfc80dd17bd38
David Bolin <david@bolin.at>
2019-03-04 12:29:40 -0800

    modified local store update method

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,8 @@ $(document).ready(function() {
     .then((items) => {
       items.forEach((item) => store.addItem(item));
       shoppingList.render();
+      
     });
 
 });
-
-api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    const item = items[0];
-    return api.updateItem(item.id, { name: 'foobar', checked: true, height: 5 });
-  })
-  .then(res => res.json())
-  .then(() => console.log('updated!'));
-
 
--- scripts/store.js
+++ scripts/store.js
@@ -11,23 +11,14 @@ const store = (function(){
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
+  const findAndUpdate = function(id, newData) {
     const item = this.findById(id);
-    item.checked = !item.checked;
+    const newItem = Object.assign(item, newData);
+    return newItem;
   };
-
+  
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
-  };
-
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
   };
 
   const toggleCheckedFilter = function() {
@@ -47,15 +38,13 @@ const store = (function(){
     items: [],
     hideCheckedItems: false,
     searchTerm: '',
-
     addItem,
     findById,
-    findAndToggleChecked,
     findAndDelete,
-    findAndUpdateName,
     toggleCheckedFilter,
     setSearchTerm,
     setItemIsEditing,
+    findAndUpdate,
   };
   
 }());
